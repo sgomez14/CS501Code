552f46fd6e70d57c08cfe6e100d6ae8f6e6888df
sgomez14 <santi09@bu.edu>
2022-03-19 13:43:31 -0400

    completed homework 5

--- HW5/QwikDrawApp_W5_P1/app/src/main/java/com/example/w5_p1/MainActivity.java
+++ HW5/QwikDrawApp_W5_P1/app/src/main/java/com/example/w5_p1/MainActivity.java
@@ -1,35 +1,61 @@
 package com.example.w5_p1;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+public class MainActivity extends AppCompatActivity
+        implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
 
+    // value used to determine whether user shook the device "significantly"
+    private static int SIGNIFICANT_SHAKE = 20000;   //tweak this as necessary
     private static final int SWIPE = 1;
     private static final int SCROLL = 2;
-    private static final int SWIPE_MIN_DISTANCE = 120;          //swiping thresholds...
-    private static final int SWIPE_MAX_OFF_PATH = 250;
-    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
-    private static final int SCROLL_MIN_DISTANCE = 100;
-    private static final int SCROLL_MAX_OFF_PATH = 120;
-    private static final int SCROLL_THRESHOLD_VELOCITY = 1000;
-    private static final String DRAWABLE_PREFIX = "index_";  //used to prefix images we want to show in our app.
+    private static final int SWIPE_MIN_DISTANCE = 50;          //swiping thresholds...
+    private static final int SWIPE_MAX_OFF_PATH = 85;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 3000;
+    private static final int SCROLL_MIN_DISTANCE = 150; // scrolling thresholds
+    private static final int SCROLL_MAX_OFF_PATH = 185;
+    private static final int SCROLL_THRESHOLD_VELOCITY = 200;
+    private static final String DRAWABLE_PREFIX = "app_";  //used to prefix images we want to show in our app.
 
     private ArrayList<Drawable> drawables;
     private int currPictureIndex;  //keeping track of which drawable is currently displayed.
 
+    // Gesture Detection
     private ImageView imgView;
+    private TextView tvNum;
     private GestureDetector GD; //consumes gesture events.
+
+    // Acceleration Variables
+    private String TAG = "ACCELERATION";
+    private float lastX, lastY, lastZ;  //old coordinate positions from accelerometer, needed to calculate delta.
+    private float acceleration;
+    private float currentAcceleration;
+    private float lastAcceleration;
+
+    //Animations
+    private Animation rotate_slide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +63,49 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         setContentView(R.layout.activity_main);
 
         imgView = (ImageView) findViewById(R.id.imgView);
+        tvNum = (TextView) findViewById(R.id.tvNum);
         GD = new GestureDetector(this, this);
         currPictureIndex = 0;
         getDrawables();
         imgView.setImageDrawable(null); //Clearing out the default image from design time.
         changePicture();        //Sets the ImageView to the first drawable in the list.
 
+        rotate_slide = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.rotate_slide_off);
+
+        // implement the listener for animation life cycle
+        rotate_slide.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                NavRight(SCROLL);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart Triggered.");
+        enableAccelerometerListening();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "onStop Triggered.");
+        disableAccelerometerListening();
+        super.onStop();
     }
 
     public void getDrawables() {
@@ -66,7 +129,88 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     public void changePicture() {
         //note, this is the preferred way of changing images, don't worry about parent viewgroup size changes.
         imgView.setImageDrawable(drawables.get(currPictureIndex));
+        tvNum.setText(Integer.toString(currPictureIndex));
     }
+
+    // enable listening for accelerometer events
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void enableAccelerometerListening() {
+        // The Activity has a SensorManager Reference.
+        // This is how we get the reference to the device's SensorManager.
+        SensorManager sensorManager =
+                (SensorManager) this.getSystemService(
+                        Context.SENSOR_SERVICE);    //The last parm specifies the type of Sensor we want to monitor
+
+
+        //Now that we have a Sensor Handle, let's start "listening" for movement (accelerometer).
+        //3 parms, The Listener, Sensor Type (accelerometer), and Sampling Frequency.
+        sensorManager.registerListener(sensorEventListener,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);   //don't set this too high, otw you will kill user's battery.
+    }
+
+    // disable listening for accelerometer events
+    private void disableAccelerometerListening() {
+
+        //Disabling Sensor Event Listener is two step process.
+        //1. Retrieve SensorManager Reference from the activity.
+        //2. call unregisterListener to stop listening for sensor events
+        //THis will prevent interruptions of other Apps and save battery.
+
+        // get the SensorManager
+        SensorManager sensorManager =
+                (SensorManager) this.getSystemService(
+                        Context.SENSOR_SERVICE);
+
+        // stop listening for accelerometer events
+        sensorManager.unregisterListener(sensorEventListener,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+    }
+
+    private final SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            // get x, y, and z values for the SensorEvent
+            //each time the event fires, we have access to three dimensions.
+            //compares these values to previous values to determine how "fast"
+            // the device was shaken.
+            //Ref: http://developer.android.com/reference/android/hardware/SensorEvent.html
+
+            float x = event.values[0];   //obtaining the latest sensor data.
+            float y = event.values[1];   //sort of ugly, but this is how data is captured.
+            float z = event.values[2];
+
+            // save previous acceleration value
+            lastAcceleration = currentAcceleration;
+
+            // calculate the current acceleration
+            currentAcceleration = x * x + y * y + z * z;   //This is a simplified calculation, to be real we would need time and a square root.
+
+            // calculate the change in acceleration        //Also simplified, but good enough to determine random shaking.
+            acceleration = currentAcceleration *  (currentAcceleration - lastAcceleration);
+
+            // if the acceleration is above a certain threshold
+            if (acceleration > SIGNIFICANT_SHAKE) {
+                Log.e(TAG, "delta x = " + (x-lastX));
+                Log.e(TAG, "delta y = " + (y-lastY));
+                Log.e(TAG, "delta z = " + (z-lastZ));
+
+                NavRight(SCROLL);
+
+
+
+            }
+            else
+                lastX = x;
+                lastY = y;
+                lastZ = z;
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 
     //////////////////////////////////////////////////////////////////////////
     //very important step, otherwise we won't be able to capture our touches//
@@ -98,11 +242,14 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     public boolean onScroll(MotionEvent startEvent, MotionEvent endEvent,
                             float velocityX, float velocityY) {
 
+        // limit the scrolling distance
         if (Math.abs(startEvent.getX() - endEvent.getX()) > SCROLL_MAX_OFF_PATH) {  return false;  }
 
         /* (0,0) is top left corner of phone.
         therefore, positive value means right to left direction */
         final float distance = startEvent.getX() - endEvent.getX();
+
+        // to distinguish from swiping, scroll should be below a particular threshold
         final boolean enoughSpeed = Math.abs(velocityX) < SCROLL_THRESHOLD_VELOCITY;
 
         if (distance > SCROLL_MIN_DISTANCE && enoughSpeed){
@@ -167,7 +314,50 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     }
 
     @Override
-    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+    public boolean onFling(MotionEvent startEvent, MotionEvent endEvent,
+                           float velocityX, float velocityY) {
+
+        //
+        if (Math.abs(startEvent.getY() - endEvent.getY()) > SWIPE_MAX_OFF_PATH) {  return false;  }
+
+
+        /* (0,0) is top left corner of phone.
+        therefore, positive value means right to left direction */
+        final float distance = startEvent.getX() - endEvent.getX();
+        final boolean enoughSpeed = Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY;
+
+        // to distinguish from scroll, swipe should be above a particular threshold
+        if (distance > SWIPE_MIN_DISTANCE && enoughSpeed){
+            // right to left scroll
+            NavRight(SWIPE);
+            return true;
+        }
+        else if (distance < -SWIPE_MIN_DISTANCE && enoughSpeed){
+            // left to right scroll
+            NavLeft(SWIPE);
+            return true;
+        }
+        else {
+            // ignore
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent motionEvent) {
+        // apply animation to both the image view and text view
+        imgView.startAnimation(rotate_slide);
+        tvNum.startAnimation(rotate_slide);
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
         return false;
     }
 }
